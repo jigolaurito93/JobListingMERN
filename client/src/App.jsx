2a619232ec0b4663ffa403dbd6cd6fc92980e81c
Jose Laurito <jigolaurito93@yahoo.com>
2024-03-25 04:59:29 -0500

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,22 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h1>home</h1>,
+  },
+  {
+    path: "/about",
+    element: (
+      <div>
+        <h1>about page</h1>,
+      </div>
+    ),
+  },
+]);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
+
+export default App;
