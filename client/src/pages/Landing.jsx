dfa7d6efd73cd1465c581dc15d6d54dd17f2b538
Jose Laurito <jigolaurito93@yahoo.com>
2024-03-26 02:53:45 -0500

    created error page

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,16 +1,36 @@
 import styled from "styled-components";
-const StyledBtn = styled.button`
-  font-size: 1.5rem;
-  background-color: red;
-  color: white;
-`;
+import Wrapper from "../assets/wrappers/LandingPage";
+import main from "../assets/images/main.svg";
+import { Link } from "react-router-dom";
+import { Logo } from "../components";
 
 const Landing = () => {
   return (
-    <div>
-      <h1>Landing Page</h1>
-      <StyledBtn>styled btn</StyledBtn>
-    </div>
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia
+            totam deleniti harum, maiores hic dolorem neque quaerat adipisci
+            consequatur maxime, nesciunt molestiae. Ipsa facilis delectus
+            laboriosam repudiandae praesentium ea molestias.
+          </p>
+          <Link to="/register" className="btn register-link">
+            Register
+          </Link>
+          <Link to="/login" className="btn">
+            Login / Demo User
+          </Link>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </Wrapper>
   );
 };
 
