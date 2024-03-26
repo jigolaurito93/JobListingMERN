c6c054a01bb9a2842f6c8b8e839be02d921c6b78
Jose Laurito <jigolaurito93@yahoo.com>
2024-03-26 03:44:32 -0500

    Dashboard structure

--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Admin = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  return <div>Admin Page</div>;
+};
 
-export default Admin
+export default Admin;
