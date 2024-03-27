4dbd9ed4a2be9568ffe7f05a4f80011d70e9d9da
Jose Laurito <jigolaurito93@yahoo.com>
2024-03-27 03:17:27 -0500

    Edit job

--- server.js
+++ server.js
@@ -60,6 +60,22 @@ app.get("/api/v1/jobs/:id", (req, res) => {
   res.status(200).json({ job });
 });
 
+// EDIT JOB
+app.patch("/api/v1/jobs/:id", (req, res) => {
+  const { company, position } = req.body;
+  if (!company || !position) {
+    return res.status(404).json({ msg: "Please provide company and position" });
+  }
+  const { id } = req.params;
+  const job = jobs.find((job) => job.id === id);
+  if (!job) {
+    return res.status(404).json({ msg: `No job with id ${id}` });
+  }
+  job.company = company;
+  job.position = position;
+  res.status(200).json({ msg: "Job modified", jobs });
+});
+
 const port = process.env.PORT || 5100;
 
 app.listen(port, (req, res) => {
