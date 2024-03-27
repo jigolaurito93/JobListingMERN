a21003e747e540210cc40a9ddb76333eac9675cf
Jose Laurito <jigolaurito93@yahoo.com>
2024-03-27 05:23:12 -0500

    get all jobs and get single job functionality

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -17,6 +17,7 @@ let jobs = [
 
 // GET ALL JOBS
 export const getAllJobs = async (req, res) => {
+  const jobs = await Job.find({});
   res.status(200).json({ jobs });
 };
 
@@ -29,9 +30,8 @@ export const createJob = async (req, res) => {
 // GET SINGLE JOB
 export const getJob = async (req, res) => {
   const { id } = req.params;
-  const job = jobs.find((job) => job.id === id);
+  const job = await Job.findById(id);
   if (!job) {
-    throw new Error("no job with that id");
     return res.status(404).json({ msg: `No job with id ${id}` });
   }
   res.status(200).json({ job });
