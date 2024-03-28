e0526c42b653ee4eb88c006d0b6be2310b0abda8
Jose Laurito <jigolaurito93@yahoo.com>
2024-03-28 03:47:58 -0500

    more custom errors

--- errors/customErrors.js
+++ errors/customErrors.js
@@ -7,3 +7,28 @@ export class NotFoundError extends Error {
     this.StatusCode = StatusCodes.NOT_FOUND;
   }
 }
+
+export class BadRequestError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "BadRequestError";
+    this.StatusCode = StatusCodes.BAD_REQUEST;
+  }
+}
+
+export class UnauthenticatedError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "UnauthenticatedError";
+    this.StatusCode = StatusCodes.UNAUTHORIZED;
+  }
+}
+
+export class Unauthorized extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "Unauthorized";
+    this.StatusCode = StatusCodes.FORBIDDEN;
+  }
+}
+
