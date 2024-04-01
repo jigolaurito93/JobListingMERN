d05f42f2f934eecae2331fda9fbf825c4b38ffb7
Jose Laurito <jigolaurito93@yahoo.com>
2024-04-01 00:48:59 -0500

    created user controller and router

---
+++ controllers/authController.js
@@ -0,0 +1,11 @@
+import User from "../models/UserModel.js";
+
+// Register a new user
+export const register = async (req, res) => {
+  res.send("register");
+};
+
+// Login a user
+export const login = async (req, res) => {
+  res.send("login");
+};
--- server.js
+++ server.js
@@ -12,6 +12,7 @@ import { body, validationResult } from "express-validator";
 
 // routers
 import jobRouter from "./routes/jobRouter.js";
+import authRouter from "./routes/authRouter.js";
 
 // middlware
 import errorHandlerMiddleware from "./middleware/errorHandlerMiddleware.js";
@@ -23,6 +24,8 @@ if (process.env.NODE_ENV == "development") {
 app.use(express.json());
 
 app.use("/api/v1/jobs", jobRouter);
+
+app.use("/api/v1/auth", authRouter);
 
 app.use("*", (req, res) => {
   res.status(404).json({ msg: "not found" });
