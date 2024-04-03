fbbc73bdf4f49aee0087c29ce8a9b815c0d4ef18
Jose Laurito <jigolaurito93@yahoo.com>
2024-04-02 23:52:53 -0500

    rename token

--- utils/tokenUtils.js
+++ utils/tokenUtils.js
@@ -3,9 +3,9 @@ import jwt from "jsonwebtoken";
 export const createJWT = (payload) => {
   // Pass in 3 things
   // Payload, secret key and an object that contains when it expires
-  const token = jwt.sign(payload, process.env.JWT_SECRET, {
+  const tokens = jwt.sign(payload, process.env.JWT_SECRET, {
     // It expires in 1 Day
     expiresIn: process.env.JWT_EXPIRES_IN,
   });
-  return token;
+  return tokens;
 };
