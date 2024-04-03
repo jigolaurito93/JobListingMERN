2b770620df33aba24825c8492f3337379d3dd313
Jose Laurito <jigolaurito93@yahoo.com>
2024-04-03 03:42:53 -0500

    Get current user and hide the password

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,8 +2,10 @@ import { StatusCodes } from "http-status-codes";
 import User from "../models/UserModel.js";
 import Job from "../models/JobModel.js";
 
-export const getCurrentUser = async (req, res, next) => {
-  res.status(StatusCodes.OK).json({ msg: "get current user" });
+export const getCurrentUser = async (req, res) => {
+  const user = await User.findOne({ _id: req.user.userId });
+  const userWithoutPassword = user.toJSON();
+  res.status(StatusCodes.OK).json({ user: userWithoutPassword });
 };
 export const getApplicationStats = async (req, res, next) => {
   res.status(StatusCodes.OK).json({ msg: "application stats" });
--- models/UserModel.js
+++ models/UserModel.js
@@ -19,4 +19,11 @@ const UserSchema = new mongoose.Schema({
   },
 });
 
+// Function to hide the password
+UserSchema.methods.toJSON = function () {
+  let obj = this.toObject();
+  delete obj.password;
+  return obj;
+};
+
 export default mongoose.model("User", UserSchema);
