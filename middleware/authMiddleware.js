a23015da504288df8746056638a768b2530f57d1
Jose Laurito <jigolaurito93@yahoo.com>
2024-04-04 01:23:47 -0500

    get application stats

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -12,3 +12,12 @@ export const authenticateUser = (req, res, next) => {
   }
   next();
 };
+
+export const authorizePermissions = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      throw new UnauthenticatedError("Unauthorized to access this route");
+    }
+    next();
+  };
+};
