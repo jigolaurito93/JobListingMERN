ca00aa01becc666572538d386d89a5c7b73e9a58
Jose Laurito <jigolaurito93@yahoo.com>
2024-04-04 01:39:52 -0500

    proxy set up

--- server.js
+++ server.js
@@ -26,6 +26,10 @@ if (process.env.NODE_ENV == "development") {
 app.use(cookieParser());
 app.use(express.json());
 
+app.get("/api/v1/test", (req, res, next) => {
+  res.json({ msg: "test" });
+});
+
 app.use("/api/v1/jobs", authenticateUser, jobRouter);
 app.use("/api/v1/users", authenticateUser, userRouter);
 app.use("/api/v1/auth", authRouter);
