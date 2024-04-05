333b749bf8d3ec66cbf1dee018dcb265fcd6402a
Jose Laurito <jigolaurito93@yahoo.com>
2024-04-05 01:46:09 -0500

    imported toastify and applied on register

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -2,6 +2,7 @@ import { Form, redirect, useNavigation, Link } from "react-router-dom";
 import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
 import { Logo, FormRow } from "../components";
 import customFetch from "../utils/customFetch";
+import { toast } from "react-toastify";
 
 export const action = async ({ request }) => {
   const formData = await request.formData();
@@ -9,9 +10,11 @@ export const action = async ({ request }) => {
 
   try {
     await customFetch.post("/auth/register", data);
+    toast.success("Registration Successful");
     return redirect("/login");
   } catch (error) {
-    console.log(error);
+    toast.error(error?.response.data?.msg);
+
     return error;
   }
 };
